3bf65ccc2da80990768b363782598869fcce87d0
ksks1234 <logging2021@gmail.com>
2024-10-26 14:38:12 +0900

    feat: entity 설정과 repository 생성 후 테스트

--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -1,5 +1,7 @@
 package com.example.firstproject.dto;
 
+import com.example.firstproject.enetity.Article;
+
 public class ArticleForm {
     private String title; // 제목을 받을 필드
     private String content; // 내용을 받을 필드
@@ -18,4 +20,11 @@ public class ArticleForm {
                 ", content='" + content + '\'' +
                 '}';
     }
+
+
+    public Article toEntity() {
+        return new Article(null, title, content);
+    }
+
+
 }
