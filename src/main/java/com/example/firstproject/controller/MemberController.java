4bcb4ba197164d091b48d33ce3b9142bff2e0c0b
ksks1234 <logging2021@gmail.com>
2024-10-28 19:41:29 +0900

    Refactor: member의 생성자와 toString 롬복대체, 컨트롤러 println을 log로 변경

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -3,11 +3,13 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.MemberForm;
 import com.example.firstproject.enetity.Member;
 import com.example.firstproject.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j
 @Controller
 public class MemberController {
 
@@ -15,19 +17,19 @@ public class MemberController {
     private MemberRepository memberRepository;
 
     @GetMapping("/signup")
-    public String signup(){
+    public String signUpPage(){
         return "members/new";
     }
 
     @PostMapping("/join")
     public String join(MemberForm form) {
-        System.out.println(form.toString());
+        log.info(form.toString());
         // 1. DTO를 엔티티로 변환
         Member member = form.toEntity();
-        System.out.println(member.toString());
+        log.info(member.toString());
         // 2. 리파지토리로 엔티티를 DB에 저장
         Member saved = memberRepository.save(member);
-        System.out.println(saved.toString());
+        log.info(saved.toString());
         return "";
     }
 
