c40d37062ac431d93968a8ba7b12bc59b5b32be8
ksks1234 <logging2021@gmail.com>
2024-11-01 18:45:34 +0900

    feat: member 1명 조회, 전체 조회 기능 추가

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -6,8 +6,13 @@ import com.example.firstproject.repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -31,6 +36,20 @@ public class MemberController {
         Member saved = memberRepository.save(member);
         log.info(saved.toString());
         return "";
+    }
+
+    @GetMapping("/members/{id}")
+    public String show(@PathVariable Long id, Model model){
+        Member member = memberRepository.findById(id).orElse(null);
+        model.addAttribute("member", member);
+        return "members/show";
+    }
+
+    @GetMapping("/members")
+    public String index(Model model){
+        ArrayList<Member> memberList = memberRepository.findAll();
+        model.addAttribute("memberList", memberList);
+        return "members/index";
     }
 
 
