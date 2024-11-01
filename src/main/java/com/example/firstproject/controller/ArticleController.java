4ddd3d671a5489ec56f2b93869e2f76a299b7a95
ksks1234 <logging2021@gmail.com>
2024-11-01 18:21:26 +0900

    feat: 해당 게시글 출력하기(select)

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -6,8 +6,12 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
 
 @Slf4j // 로깅 기능을 위한 어노테이션 추가, println()문 대체
 @Controller
@@ -32,5 +36,19 @@ public class ArticleController {
         log.info(saved.toString());
 //        System.out.println(saved.toString());
         return "";
+    }
+
+    @GetMapping("/articles/{id}") // 데이터 조회 요청 접수
+    public String show(@PathVariable Long id, Model model) { // 매개변수로 id 받아 오기
+        log.info("id = " + id); // id를 잘 받았는지 확인하는 로그 찍기
+        // 1. id를 조회해 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null); // 해당 id 값이 없으면 null을 반환한다.
+        // 2. 모델에 데이터 등록하기
+        // name 이라는 이름으로 value 객체 추가
+        // 형식 : model.addAttribute(String name, Object value);
+        model.addAttribute("article", articleEntity);
+
+        // 3. 뷰 페이지 반환하기
+        return "articles/show";
     }
 }
