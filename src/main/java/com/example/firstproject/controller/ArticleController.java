2cc34d75fff1bce492e00bd49a254bea79a6875c
ksks1234 <logging2021@gmail.com>
2024-11-02 19:24:36 +0900

    feat: 페이지 링크 연결

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -36,7 +36,7 @@ public class ArticleController {
         Article saved = articleRepository.save(article);    // article 엔티티를 저장해 saved 객체에 반환
         log.info(saved.toString());
 //        System.out.println(saved.toString());
-        return "";
+        return "redirect:/articles/" + saved.getId();
     }
 
     @GetMapping("/articles/{id}") // 데이터 조회 요청 접수
--- src/main/java/com/example/firstproject/enetity/Article.java
+++ src/main/java/com/example/firstproject/enetity/Article.java
@@ -4,13 +4,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 @AllArgsConstructor
 @NoArgsConstructor // 기본 생성자 추가 어노테이션
 @ToString
+@Getter
 @Entity
 public class Article {
     @Id
@@ -20,4 +19,5 @@ public class Article {
     private String title;
     @Column
     private String content;
+
 }
