2c3449fd8fef3c9037efb144ce00fc5447b62cc0
ksks1234 <logging2021@gmail.com>
2024-11-07 00:11:42 +0900

    Refactor: 게시글 수정 기능 refactor

--- src/main/java/com/example/firstproject/service/ArticleService.java
+++ src/main/java/com/example/firstproject/service/ArticleService.java
@@ -3,13 +3,16 @@ package com.example.firstproject.service;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.enetity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @Service // 서비스 객체 선언
 public class ArticleService {
     @Autowired
@@ -31,6 +34,23 @@ public class ArticleService {
         return articleRepository.save(article);
     }
 
+    public Article update(Long id, ArticleForm dto) {
+        // 1. DTO -> 엔티티 변환하기
+        Article article = dto.toEntity();
+        log.info("id: {}, article: {}", id, article.toString());
+        // 2. 타깃 조회하기
+        Article target = articleRepository.findById(id).orElse(null);
+        // 3. 잘못된 요청 처리하기
+        if (target == null || id != article.getId()) {
+            // 400, 잘못된 요청 응답!
+            log.info("잘못된 요청! id: {}, article: {}", id, article.toString());
+            return null; // 응답은 컨트롤러가 하므로 여기서는 null을 반환
+        }
+        // 4. 업데이트 및 정상 응답(200)하기
+        target.patch(article);
+        Article updated = articleRepository.save(target);
+        return updated; // 응답은 컨트롤러가 하므로 여기서는 수정 데이터만 반환
+    }
 
 
 
